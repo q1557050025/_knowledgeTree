62e6596f03809c074fd7f95f27127449263c48d8
Buke <43104602+q1557050025@users.noreply.github.com>
2019-03-26 19:37:16 +0800

    2019-03-26 19:37:13

---
+++ web/webpack-about/src/index.js
@@ -0,0 +1,11 @@
+import _ from 'lodash';
+import './style.css'
+
+function component() {
+	var element = document.createElement('div');
+	element.innerHTML = _.join(["hello", "webpack"], ' ');
+	element.classList.add('hello');
+	return element;
+}
+
+document.body.appendChild(component());
