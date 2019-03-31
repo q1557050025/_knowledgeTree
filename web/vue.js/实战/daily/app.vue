4011e81e2004cbe68664f195a00a1c3f4635bd83
Buke <43104602+q1557050025@users.noreply.github.com>
2019-04-01 03:54:24 +0800

    2019-04-01 03:54:12

--- web/vue.js/实战/daily/app.vue
+++ web/vue.js/实战/daily/app.vue
@@ -1,25 +1,78 @@
 <template>
     <div>
-        <v-title title="Vue组件化"></v-title>
-        <v-button @click="handleClick">点击按钮</v-button>
-        <p>
-            <img src="./images/image.png" style="width: 200px;">
-        </p>
+        <div class="daily">
+            <div class="daily-menu">
+                <div 
+                    class="daily-menu-item"
+                    :class="{on: type === 'recommend'}"
+                >每日推荐</div>
+                <div 
+                    class="daily-menu-item"
+                    :class="{on: type === 'daily'}"
+                    @click="showThemes = !showThemes"
+                >主题日报</div>
+                <ul v-show="showThemes">
+                    <li v-for="(theme, index) in themes" :key="index">
+                        <a
+                            :class="{on: theme.id === themeID && type === 'daily'}"
+                            @click="handleToTheme(theme.id)"
+                        >{{theme.name}}</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="daily-list">
+                <Item></Item>
+            </div>
+            <daily-article></daily-article>
+        </div>
     </div>
 </template>
-<script>
-    import vTitle from './title.vue';
-    import vButton from './button.vue';
 
+<script>
+    import $ from './libs/util'
     export default {
-        components: {
-            vTitle,
-            vButton
+        data() {
+            return {
+                themes: [],
+                showThemes: false,
+                type: 'recommend',
+                themeId: 0,
+            }
         },
         methods: {
-            handleClick (e) {
-                console.log(e);
+            getThemes() {
+                $.ajax.get('themes').then(res => {
+                    this.themes =res.others;
+                })
+            },
+            handleToTheme() {
+                this.type = 'daily';
+                this.themeId = id;
+                this.list = [];
+                $.ajax.get('theme/'+id).then(res => {
+                    this.list = res.stories
+                        .fileter(item => item.type !== 1);
+                })
+            },
+            handleToRecommend() {
+                this.type = 'recommend';
+                this.recommendList = [];
+                this.dailyTime = $.getTodayTime();
+                this.getRecommendList();
+            },
+            getRecommendList() {
+                this.isLoading = true;
+                const prevDay = $.prevDay(this.dailyTime + 86400000);
+                $.ajax.get('news/before' + prevDay).then(res => {
+                    this.recommendList.push(res);
+                    this.isLoading = false;
+                })
             }
+        },
+        mounted() {
+            this.getThemes();
+            this.getRecommendList();
         }
     }
 </script>
+
---
+++ web/vue.js/实战/shopping/main.js
@@ -0,0 +1,79 @@
+import Vue from 'Vue';
+import VueRouter from 'vue-router';
+import Routers from './router.js';
+import Vuex from 'vuex';
+import App from './app.vue';
+import './style.css';
+import product_data from './product.js';
+
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const RouterConfig = {
+	mode: 'history',
+	routes: Routers
+};
+
+const router = new VueRouter(RouterConfig);
+
+router.beforeEach((to, from, next) => {
+	window.document.title = to.meta.title;
+	next();
+});
+
+router.afterEach((to, from, next) => {
+	window.scrollTo(0,0);
+});
+// 数组排重
+function uniqueArray (array) {
+    const res = [];
+    const json = {};
+    for (let i = 0; i < array.length; i++){
+        const _self = array[i];
+        if(!json[_self]){
+            res.push(_self);
+            json[_self] = 1;
+        }
+    }
+    return res;
+}
+
+const store = new Vuex.Store({
+	state: {
+		productList: [],
+		cartList: [],
+	},
+	getters: {
+		brands: state => {
+			//.map返回每次调用函数返回的结果组成的数组
+			const brands = state.productList.map(item => item.brand);
+			return uniqueArray(brands);
+		},
+		colors: state => {
+			//.map返回每次调用函数返回的结果组成的数组
+			const colors = state.productList.map(item => item.color);
+			return uniqueArray(colors);
+		},
+	},
+	mutations: {
+		setProductList(state, data) {
+			state.productList = data;
+		}
+	},
+	actions: {
+		getProductList(context) {
+			setTimeout(() => {
+				context.commit('setProductList', product_data);
+			}, 1000);
+		}
+	}
+});
+
+new Vue({
+	el: '#app',
+	router: router,
+	store,
+	render: h => {
+		return h(App)
+	}
+});
